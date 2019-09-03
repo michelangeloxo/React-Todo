7accc0d66a823b4d4cda62167e6812bc0e8771ea
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-03 00:10:48 -0700

    Commits 1-4

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,35 @@
+import React from 'react'; 
+
+class TodoForm extends React.Component {
+
+constructor(props) {
+
+super(props);
+this.state= {
+    newtodo: ""
+};
+}
+
+handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value }); 
+}
+
+handleSubmit = event => {
+
+event.preventDefault();
+this.props.addTodo(this.state.newtodo); 
+
+}
+
+render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <input type='text' name='newtodo' placeholder='Add a new todo' onChange={this.handleChange} value={this.state.newtodo} />
+            <button type='submit'>Add Todo</button>
+        </form>
+    ); 
+}
+}
+
+
+export default TodoForm; 
